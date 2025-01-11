0058304e74a196a3b943bebee48915c78bc893a5
lajor <konnichiwarojal@gmail.com>
2025-01-11 21:17:04 +0545

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
         <Route path="/panda/" element={<RootLayout />}>
           <Route index element={<Envelope />} />
           <Route path="home" element={<Home />} />
+          <Route path="*" element={<Envelope />} />
         </Route>
       ]
     )
